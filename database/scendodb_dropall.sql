5e2c2a860a963b8268b63770a62f39c728122d00
Raffaele <raffio98@gmail.com>
2022-06-10 22:29:08 +0200

    project ready to start

--- database/scendodb_dropall.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE Friendships;
-DROP TABLE User_Scendo;
-DROP TABLE Users;
-DROP TABLE Scendos;
---
+++ src/test/java/com/scendodevteam/scendo/ScendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scendodevteam.scendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
