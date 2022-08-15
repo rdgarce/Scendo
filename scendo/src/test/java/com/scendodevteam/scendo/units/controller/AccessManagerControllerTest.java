321272624e3096cf0e7b7b876e9af331a99b1fff
Raffaele <raffio98@gmail.com>
2022-08-15 21:59:42 +0200

    multiple profile added

--- scendo/src/test/java/com/scendodevteam/scendo/units/controller/AccessManagerControllerTest.java
+++ scendo/src/test/java/com/scendodevteam/scendo/units/controller/AccessManagerControllerTest.java
@@ -39,8 +39,6 @@ import com.scendodevteam.scendo.service.UtenteSC;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
-
 @SpringBootTest
 @TestInstance(Lifecycle.PER_CLASS)
 @AutoConfigureMockMvc
